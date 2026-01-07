8ee07ea2a7d06a44a20518bc105b763bfe5d05f1
ajone1111 <adjust.made@gmail.com>
2026-01-06 21:49:18 -0800

    18. Load Accounts Pt 1.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@react-native-async-storage/async-storage': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
