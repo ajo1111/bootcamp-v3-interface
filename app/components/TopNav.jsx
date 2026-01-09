575f3c6ea45fb22b35b2f04c1391fd5cb761433c
ajone1111 <adjust.made@gmail.com>
2026-01-08 19:40:10 -0800

    19. Load Accounts Pt.2

--- app/components/TopNav.jsx
+++ app/components/TopNav.jsx
@@ -5,13 +5,18 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Image from "next/image";
 import { useSDK } from "@metamask/sdk-react";
+import Jazzicon from "react-jazzicon";
 import { ethers } from "ethers";
+
 
 // Import hooks
 import { useProvider } from "../hooks/useProvider";
 
 //Import assets
 import network from "@/app/assets/other/network.svg";
+
+// Import config
+import config from "@/app/config.json";
 
 function TopNav() {
 
@@ -30,23 +35,47 @@ function TopNav() {
         }
     }
 
-    
+    async function networkHandler() {
+        await metamask.request({
+            method: "wallet_switchEthereumChain",
+            params: [{ chainId: e.target.value }],
+        });
+    }    
+
     async function getAccountInfo() {
-        if (!provider) return;
-        
         // Get the currently connected account & balance
         const account = await provider.getSigner()
         const balance = await provider.getBalance(account)
         
         //Store the values in state
         setAccount(account.address)
-        setBalance(balance)
+        setBalance(ethers.formatUnits(balance, 18))
     }
 
-    useEffect(() => {
-        // // connect to blockchain here...
-        // connecthandler();
-    }, [provider]);
+useEffect(() => {
+
+    if(sdk && metamask) {
+        // // create event listener
+        metamask.on("accountsChanged", async (accounts) => {
+        if (accounts.length === 0) {
+            // no accounts are connected
+            setAccount("");
+            setBalance("");
+        } else {
+          await getAccountInfo();
+        }
+    });
+
+    metamask.on("chainChanged", () => window.location.reload());
+
+    //This allows us to remove any duplicate event
+    // Listeners that may be added from navigating
+    // back and forth to this page
+    return () => {
+        metamask.removeAllListeners();
+    }
+    }
+}, [sdk, metamask, provider]);
 
     return(
         <nav className="topnav">
@@ -55,11 +84,41 @@ function TopNav() {
                 <Image src={network} alt="Select network" />
                </label>
             <div className="select">
-                <select name="network" id="network" value="">
+                <select 
+                name="network" 
+                id="network" 
+                value={config[Number(chainId)] ? chainId : "0x7a69"}
+                onChange={networkHandler}
+                >
                    <option value="0">Select</option>
                    <option value="0x7a69">Hardhat</option>
                 </select>
                 </div> 
+            </div>
+
+            <div className="account">
+                {account && (
+                <div className="balance">
+                    <p>My Balance <span>{Number(balance).toFixed(2)} ETH</span></p>
+                </div>
+                )}
+
+                {account ? (
+                <a 
+                  href={`https://etherscan.io/address/${account}`}
+                  target="_blank" 
+                  rel="noreferrer"
+                  className="link"
+                >
+                  {account.slice(0,6) + account.slice(38, 42)}
+                  <Jazzicon diameter={44} seed={account} />
+                </a>
+                ) : (
+                  <button onClick={connecthandler} className="button">
+                    Connect
+                    </button>
+                )}
+
             </div>
         </nav>
     );
